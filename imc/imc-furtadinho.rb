2a0fe9b020c492d74ebc81cb3537365c7b6254c8
Code Golf <projetos.em.python@gmail.com>
2017-01-11 14:53:11 -0200

    Update imc-furtadinho.rb

--- imc/imc-furtadinho.rb
+++ imc/imc-furtadinho.rb
@@ -1,1 +1,1 @@
-t="";print "IMC: %.2f"%m=gets.to_i/gets.to_f**2,"\nSituacao: ",m<20? "Abaixo do Peso":t,20<=m&&m<25? "Peso Normal":t,25<=m&&m<30? "Acima do Peso":t,30<=m&&m<34? "Obeso":t,m>=34? "Muito Obeso":t
+puts "IMC: %.2f"%m=gets.to_i/gets.to_f**2,"Situacao: #{m<20? "Abaixo do Peso":19<m&&m<25? "Peso Normal":24<m&&m<30? "Acima do Peso":29<m&&m<34? "Obeso":m>33? "Muito Obeso":""}"
