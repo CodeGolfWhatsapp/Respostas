d3f506a6dadb951a5b07afb69f15a42b4897be5e
Code Golf <projetos.em.python@gmail.com>
2017-01-20 18:08:04 -0200

    Create viagemKemper.py

---
+++ viagem/viagemKemper.py
@@ -0,0 +1,1 @@
+c,k="combustivel:",input("Informe a distancia em KM: ");l=int(k/input("Informe o consumo medio: ")+.5);print"Total em Litros de",c,l,"Litros\nTotal gasto com",c,"R$ %.2f\nParadas previstas:"%(l*3.8),k/250
