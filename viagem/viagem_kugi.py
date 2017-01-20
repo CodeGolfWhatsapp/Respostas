8e5b9a4cd0f16955c9fba1ee1e17eef123be160b
Code Golf <projetos.em.python@gmail.com>
2017-01-20 19:07:47 -0300

    3º Lugar no Ranking - 208 Bytes

---
+++ viagem/viagem_kugi.py
@@ -0,0 +1,1 @@
+k=input('Informe a distancia em Km: ');l=round(k/input('Informe o consumo medio: '));g=l*3.8;p=k/250;print'Total em litros de combustivel:',l,'Litros\nTotal gasto com combustivel:',g,'\nParadas previstas:',p
