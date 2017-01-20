0a9039e4200ea1ebb91a8ba2cea00a2e1217451c
Code Golf <projetos.em.python@gmail.com>
2017-01-20 18:25:43 -0200

    Código de Jorge Eduardo em Python

---
+++ viagem/viagem-JorgeEduardo.py
@@ -0,0 +1,6 @@
+d=int(input('Informe a distancia em Km: '))
+c=float(input('Informe o consumo medio: '))
+l=round(d/c);g=l*3.8;p=d/250
+print('Total de litros de combustivel: %d litros'%l)
+print('Total gasto com combustivel: R$ %.2f'%g)
+print('Paradas previstas: %d'%p)
