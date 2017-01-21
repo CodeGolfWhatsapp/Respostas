aeb62e3036c40560202bd01c85e10ff0927cdeb8
Code Golf <projetos.em.python@gmail.com>
2017-01-21 08:37:57 -0300

    Criado por Renan. 206 Bytes
    206 Bytes

---
+++ viagem/viagemRenan.py
@@ -0,0 +1,4 @@
+c=' combustivel: '
+s=input('Informe a distancia em Km: ')
+v=round(s/input('Informe o consumo medio: '))
+print'Total em Litros de%s%d Litros\nTotal gasto com%sR$ %s0\nParadas previstas:'%(c,v,c,v*3.8),s/250
