54e0776e2124c02ac2aa48b35af9516861d753b7
Code Golf <projetos.em.python@gmail.com>
2017-01-21 16:43:16 -0200

    Delete Deivison viagem.php

--- viagem/Deivison viagem.php
+++
@@ -1,1 +0,0 @@
-<?php echo"Informe a distancia em KM: ";$a=fgets(STDIN);echo"Informe o consumo medio: ";$c=intval($a/fgets(STDIN)+.5);echo"Total em Litros de: $c litros\nTotal gasto com combustivel: R$ ".number_format($c*3.8,2)."\nParadas previstas: ".intval($a/250);
