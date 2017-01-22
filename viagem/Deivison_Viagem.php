1a5d543172a2ca8112a1ccccf73b23688d815728
Code Golf <projetos.em.python@gmail.com>
2017-01-21 23:41:16 -0200

    Update Deivison_Viagem.php

--- viagem/Deivison_Viagem.php
+++ viagem/Deivison_Viagem.php
@@ -1,1 +1,1 @@
-<?php $m="combustivel";echo"Informe a distancia em KM: ";$a=fgets(STDIN);echo"Informe o consumo medio: ";$c=$a/fgets(STDIN)+.5;echo sprintf("Total em Litros de $m: %u litros\nTotal gasto com $m: R$ %.2f\nParadas previstas: %u",$c,$c*3.8,$a/250);
+<?php $m="combustivel";echo"Informe a distancia em KM: ";$a=fgets(STDIN);echo"Informe o consumo medio: ";$c=intval($a/fgets(STDIN)+.5);echo sprintf("Total em Litros de $m: %u litros\nTotal gasto com $m: R$ %.2f\nParadas previstas: %u",$c,$c*3.8,$a/250);
