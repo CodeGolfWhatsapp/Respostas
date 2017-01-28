7c597ebf2f86d767c1ebd70d091f06a627acc757
Code Golf <projetos.em.python@gmail.com>
2017-01-28 16:13:47 -0200

    Delete Baidu_NiverCaculator.js

--- Niver/Baidu_NiverCaculator.js
+++
@@ -1,1 +0,0 @@
-p=prompt,f="Informe a data ",n=p(f+"atual: "),n+="/"+p(f+"de nascimento: "),n=n.split`/`,u=n[0],d=n[1],q=n[3],c=n[4],m=d-c,d=u-q,y=n[2]-n[5],i=m*30+d,d<c||d==c&&u<q?0:y--,i=u>q?360-i:i,i<0?i*=-1:0;alert(`Idade: ${y} Anos\nFaltam ${i} dias, ${i?"espere":"Parabéns"}!`)
