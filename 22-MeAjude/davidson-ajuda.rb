310aff8ae8d1928fa2918a8053671499a91ae17c
Code Golf <projetos.em.python@gmail.com>
2017-04-21 18:28:45 -0300

    Update davidson-ajuda.rb

--- 22-MeAjude/davidson-ajuda.rb
+++ 22-MeAjude/davidson-ajuda.rb
@@ -1,1 +1,1 @@
-a,b=c=[0,0].map{eval gets.tr' ',?*};puts"#{a==b ?'Empate':a>b ?'Primeiro':'Segundo'}\nMaior area: #{c.max}"
+a,b=c=[0,0].map{eval gets.tr' ',?*};puts a==b ?'Empate':"#{a>b ?'Primeiro':'Segundo'}\nMaior area: #{c.max}"
