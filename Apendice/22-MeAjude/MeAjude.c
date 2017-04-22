0aae42f609f43875682c3c4a1b6f8a3385f1faad
Code Golf <projetos.em.python@gmail.com>
2017-04-21 22:22:25 -0300

    Me ajude - 183 Bytes - C

---
+++ Apendice/22-MeAjude/MeAjude.c
@@ -0,0 +1,1 @@
+main(){int A,B,C,D;scanf("%d%d%d%d",&A,&B,&C,&D);if (A*B>C*D)printf("nPrimeiro\nMaior area: %d",A*B);if (A*B<C*D)printf("Segundo\nMaior area: %d",C*D);if (A*B==C*D)printf("Empate");}
