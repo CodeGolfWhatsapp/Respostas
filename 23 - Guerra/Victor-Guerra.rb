ead237e569e278b0815834898f063fe9f686c5f1
Code Golf <projetos.em.python@gmail.com>
2017-04-30 23:24:39 -0300

    Victor-Guerra.rb

--- 23 - Guerra/Victor-Guerra.rb
+++ 23 - Guerra/Victor-Guerra.rb
@@ -1,1 +1,9 @@
-t=eval(gets.tr("12","67").gsub(" 3","-5").tr("3 ","0+").gsub("7","13"));puts t>24?"Paz Mundial!\nQuantidade de tijolos:#{t}":"Guerraaaa!"
+c=t=0
+while(a=gets.to_i)
+a<2?t+=6:
+a>2?t-=5:t+=13
+t=0 if t<0
+break if t>24||c>1
+c+=1
+end
+puts t>24 ?"Paz Mundial!\nQuantidade de tijolos:#{t}":"Guerraaaa!"
