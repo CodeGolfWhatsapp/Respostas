e7e3dfb609b467b6483e3bda8b10e76ca0cf9bcf
Code Golf <projetos.em.python@gmail.com>
2017-04-30 21:05:27 -0300

    Delete main.c

--- 23 - Guerra/main.c
+++
@@ -1,2 +0,0 @@
-#include <stdlib.h>
-int main(){int a=3,b=10,c=-5,d=1,v,t=0;while(3>=d){scanf("%d",&v);if(v==1){t+=6;}else if(v==2){t+=13;}else if(v==3){t-=5;}d++;}if(t>25){printf("%dPaz Mundial!\n",t);}if(t<25){printf("%dGerra!",t);}return 0;}
