4250947c28574064e2ab5e15928cb2ad6c07b3b8
Code Golf <projetos.em.python@gmail.com>
2017-05-07 13:32:51 -0300

    Add files via upload

---
+++ 024-professor/25-Professor-Neto_Alves.py
@@ -0,0 +1,32 @@
+﻿#python 3.6
+
+def entrada(valor):
+	valor = int(valor)
+	if 1 <= valor and valor <= 100000:
+		return valor
+	return None
+
+def soma(valorA, valorB):
+	print("SOMA = %d" % (entrada(valorA) + entrada(valorB)))
+
+def multiplicacao(valorA, valorB):
+	print("MULTIPLICACAO = %d" % (entrada(valorA) * entrada(valorB)))
+
+
+def subtracao(valorA, valorB):
+	print("SUBTRACAO = %d" % (entrada(valorA) - entrada(valorB)))
+
+def contas(valorA, valorB):
+	valorA = entrada(valorA)
+	valorB = entrada(valorB)
+	if valorA != None and valorB != None:
+		if valorA >= valorB:
+			soma(valorA, valorB)
+			multiplicacao(valorA, valorB)
+			subtracao(valorA, valorB)
+
+entradas = input()
+entradas = entradas.split(" ")
+valorA = entradas[0]
+valorB = entradas[1]
+contas(valorA, valorB)
