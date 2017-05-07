f972f3431ab950359ec672942ac275bada75b2f0
Code Golf <projetos.em.python@gmail.com>
2017-05-07 11:37:16 -0300

    Add files via upload

---
+++ 024-professor/24-Professor-Jamille_Bittencourt.java
@@ -0,0 +1,1 @@
+import java.util.*;public class M{public static void main(String[]args){Scanner s=new Scanner(System.in);int a=s.nextInt(),b=s.nextInt();System.out.print("SOMA = "+(a+b)+"\nMULTIPLICACAO = "+(a*b)+"\nSUBTRACAO = "+(a-b));}}
