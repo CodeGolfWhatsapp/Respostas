4f118ab9ddca8ccb0b69165ed5c4c7b3546876e6
Code Golf <projetos.em.python@gmail.com>
2017-05-13 11:12:38 -0300

    Rename 25-Professor-Ana Lina.java to 24-Professor-Ana Lina.java

--- 024-professor/25-Professor-Ana Lina.java
+++
@@ -1,1 +0,0 @@
-﻿import java.util.*;public class C{public static void main(String[]args){Scanner scanner=new Scanner(System.in);int n,n2,s,m,su;n=scanner.nextInt();n2=scanner.nextInt();s=n+n2;m=n*n2;su=n-n2;System.out.println("SOMA = "+s +"\n"+"MULTIPLICACAO = "+m+"\n"+"SUBTRACAO = "+su+"\n");}}
