8bdec6464e541048ea44e4fbb14e0198f783913a
Code Golf <projetos.em.python@gmail.com>
2017-05-23 16:42:45 -0300

    Add files via upload

---
+++ 026-Alunos/26-Alunos-Bruno_Lima.rb
@@ -0,0 +1,13 @@
+﻿a=Array.new
+a[0]=gets
+a[1]=gets
+a[2]=gets
+m=(a[1].to_f+a[2].to_f)/2
+$t=m<=7
+puts"NOME: "+a[0]
+puts"NOTA 1: "+a[1]
+puts"NOTA 2: "+a[2]
+puts"MEDIA: "+m.to_s
+print"STATUS: "
+print"REPROVADO"if $t
+print"APROVADO"unless $t
