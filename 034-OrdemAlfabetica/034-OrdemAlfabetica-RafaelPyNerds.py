d322d09cc37aa386da07de4383e43ef27c1375dc
Code Golf <projetos.em.python@gmail.com>
2017-07-16 19:01:39 -0300

    Update 034-OrdemAlfabetica-RafaelPyNerds.py

--- 034-OrdemAlfabetica/034-OrdemAlfabetica-RafaelPyNerds.py
+++ 034-OrdemAlfabetica/034-OrdemAlfabetica-RafaelPyNerds.py
@@ -1,4 +1,5 @@
 l=input().split();t=len(l)
-for i in range(t-1):
-    if l[i]>l[i+1]:x=l[i+1];l[i+1]=l[i];l[i]=x
+for j in range(t):
+    for i in range(t-1):
+        if l[i]>l[i+1]:x=l[i+1];l[i+1]=l[i];l[i]=x
 print(l)
