e1fcb48fd8eccb6918aca1b93277f49fd3a7e0fb
Code Golf <projetos.em.python@gmail.com>
2017-07-16 06:21:31 -0300

    Add files via upload

---
+++ 034-OrdemAlfabetica/034-OrdemAlfabetica-JorgeEduardo.py
@@ -0,0 +1,7 @@
+a,b,c=input().split()
+if a<b and b<c:print(a,b,c)
+if a<c and c<b:print(a,c,b)
+if b<a and a<c:print(b,a,c)
+if b<c and c<a:print(b,c,a)
+if c<a and a<b:print(c,a,b)
+if c<b and b<a:print(c,b,a)
