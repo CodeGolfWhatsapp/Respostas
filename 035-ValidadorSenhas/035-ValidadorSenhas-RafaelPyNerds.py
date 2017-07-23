aeae3881d4cd3ffbeded67646e5c492f9d879cde
Code Golf <projetos.em.python@gmail.com>
2017-07-23 15:38:16 -0300

    Update 035-ValidadorSenhas-RafaelPyNerds.py

--- 035-ValidadorSenhas/035-ValidadorSenhas-RafaelPyNerds.py
+++ 035-ValidadorSenhas/035-ValidadorSenhas-RafaelPyNerds.py
@@ -1,1 +1,1 @@
-import re;print('SENHA '+(''if re.match(r'[A-Za-z0-9\d][^\W_]{6,32}',input())else'IN')+'VALIDA')
+import re;print('SENHA '+(''if re.match('[A-Za-z0-9\d][^\W_]{6,32}',input())else'IN')+'VALIDA')
