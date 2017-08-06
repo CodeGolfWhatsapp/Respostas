42b9237855af21ed3a755e543df3d58a588dc570
Code Golf <projetos.em.python@gmail.com>
2017-08-06 11:59:23 -0300

    Add files via upload

---
+++ 037-CoordenadasDeUmPonto/CoordenadasPonto.lua
@@ -0,0 +1,12 @@
+x,y=io.read("*n","*n")
+if x>0 and y>0 then
+print("Q1")
+elseif x<0 and y>0 then
+print("Q2")
+elseif x<0 and y<0 then
+print("Q3")
+elseif x>0 and y<0 then
+print("Q4")
+else
+print("ORIGEM")
+end
