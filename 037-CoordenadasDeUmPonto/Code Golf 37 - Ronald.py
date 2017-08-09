60b0debdabb326bbfea3fdb2d73d53617155690f
Code Golf <projetos.em.python@gmail.com>
2017-08-08 21:08:53 -0300

    Add files via upload

---
+++ 037-CoordenadasDeUmPonto/Code Golf 37 - Ronald.py
@@ -0,0 +1,17 @@
+'''
+p=print;x,y=map(float,input().split())
+if x>0and y>0:p('Q1')
+elif x<0and y>0:p('Q2')
+elif x<0and y<0:p('Q3')
+elif x>0and y<0:p('Q4')
+elif x==y==0:p('Origem')
+elif x==0:p('Eixo X')
+else:y==0,p('Eixo Y')
+'''
+'''
+#print('QI' if  x>0 and y>0 'Eixo Y',else y==0)
+x = 10
+print ("par" if x % 2 == 0 else "impar")
+x = input()
+print('blz' if x % 2 ==0 else 'no blz')
+'''
